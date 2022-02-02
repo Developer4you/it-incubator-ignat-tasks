3652351f8e7a98e869cf701f15bbf4219f8b1c58
Developer4you <avervalpet@gmail.com>
2022-02-02 19:00:38 +0300

    fix: fix routing

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Junior() {
+    return (
+        <div>
+            <img src="https://www.tots100.co.uk/wp-content/uploads/2012/12/shutterstock_70031074.jpg" alt="junior" />
+        </div>
+    )
+}
+
+export default Junior
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function JuniorPlus() {
+    return (
+        <div>
+            <img src="https://static.103.by/images/common/wysiwyg/2019/05/344626fa70bc4ec39bd25756ee8695e6.jpg" alt="junior" />
+        </div>
+    )
+}
+
+export default JuniorPlus
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
