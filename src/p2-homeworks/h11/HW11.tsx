0e76f16de5abdfd825b8b9b171c27ca1a855c446
Developer4you <avervalpet@gmail.com>
2022-07-13 08:43:04 +0300

    add the 11th task

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,23 +6,43 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const changeValue = (value: number) => {
+        if ( value>=value2) setValue2(value+1)
+        setValue1(value)
+    }
+
+    const changeValues = (values: [number, number])=> {
+        if (values[1]>value1) setValue2(values[1])
+        if ( values[0]>=value2) setValue2(values[0]+1)
+        setValue1(values[0])
+
+    };
+
+
     return (
         <div>
             <hr/>
+
+
             homeworks 11
 
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    value = {value1}
+                    onChangeRange={changeValue}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
+
                 <SuperDoubleRange
                     // сделать так чтоб value1 и value2 изменялось
+                    onChangeRange={changeValues}
+                    value={[value1,value2]}
                 />
                 <span>{value2}</span>
             </div>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import s from './SuperDoubleRange.module.css'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
+    onChangeRange?: (values: [number, number]) => void
     value?: [number, number]
     // min, max, step, disable, ...
 }
@@ -10,14 +11,38 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
         // min, max, step, disable, ...
+        ...restProps
     }
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    const onChangeCallbackForUpperInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const value1 = value ? value[0]:0
+        onChangeRange && onChangeRange([value1, +e.currentTarget.value])
+    }
+    const onChangeCallbackForLowerInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const value2 = value ? value[1]:0
+        onChangeRange && onChangeRange([+e.currentTarget.value, value2])
+    }
+    const finalRangeClassName1 = `${s.range} ${s.upperInputClassName}`
+    const finalRangeClassName2 = `${s.range} ${s.lowerInputClassName}`
     return (
-        <>
-            DoubleRange
-        </>
+        <div className={s.wrap}>
+            <input
+                type={'range'}
+                onChange={onChangeCallbackForUpperInput}
+                className={finalRangeClassName2}
+                value={value && value[1]}
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+            <input
+                type={'range'}
+                onChange={onChangeCallbackForLowerInput}
+                className={finalRangeClassName1}
+                value={value && value[0]}
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+        </div>
     )
 }
 
